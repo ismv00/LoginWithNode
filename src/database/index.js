35f27d58e188be81b499b7a496f5a583b0318d67
Igor Menezes <ismv00@icloud.com>
2022-03-22 19:06:12 -0400

    create-file-.env

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect('mongodb+srv://ismv00:Igor2502@nodelogin.7dwic.mongodb.net/loginWithNode?retryWrites=true&w=majority');
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@nodelogin.7dwic.mongodb.net/loginWithNode?retryWrites=true&w=majority`);
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
