8d4323c03412d3b9306a6ae8aa9dec64a1057392
Igor Menezes <ismv00@icloud.com>
2022-03-22 23:26:00 -0400

    autentication with token

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const authConfig = require('../config/auth.json');
 
 const router = express.Router();
+
+function generateToken(params = { }) {
+    return jwt.sign(params, authConfig.secret, {
+        expiresIn: 86400,
+    })
+}
 
 router.post('/register', async (req, res) => {
     const { email } = req.body;
@@ -16,10 +25,31 @@ router.post('/register', async (req, res) => {
 
         user.password = undefined;
        
-        return res.send({ user });
+        return res.send({ 
+            user,
+            token: generateToken({ id: user.id }),
+        });
     } catch (err) {
         return res.status(400).send({ error: 'Registration failed' });
     }
 });
+
+router.post('/authenticated', async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select('+password');
+
+    if(!user) 
+        return res.status(400).send({ error: 'User not found' });
+    if(!await bcrypt.compare(password, user.password))
+        return res.status(400).send({ error: 'Invalid Password'});
+
+    user.password = undefined;
+
+    res.send({ 
+        user, 
+        token: generateToken({ id: user.id }) 
+    });
+})
 
 module.exports = app => app.use('/auth', router);
